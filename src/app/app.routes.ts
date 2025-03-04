8a1de3f6e57285ec00999cb22a43990b0682c77b
Midhun Ambadan <midhunambadan06@gmail.com>
2025-03-04 11:07:06 +0530

    routes and navigation done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './core/pages/home/home.component';
+import { LoginComponent } from './core/pages/login/login.component';
+import { AboutComponent } from './core/pages/about/about.component';
+import { NewsComponent } from './core/pages/news/news.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'home',
+        pathMatch:'full'
+    },
+    {
+        path:'home',
+        component:HomeComponent,
+    },
+    {
+        path:'login',
+        component:LoginComponent,
+    },
+    {
+        path:'about',
+        component:AboutComponent
+    },
+    {
+        path:'news',
+        component:NewsComponent
+    },
+
+];
---
+++ src/app/core/pages/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeaderComponent,RouterLink,RouterOutlet],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+
+  weatherData:any[]=[]
+
+  
+
+}
